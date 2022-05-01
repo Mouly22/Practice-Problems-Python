36b39626ff717cbdddc41bbb8c85f93e0534de76
Mouly <abiraazmary22@gmail.com>
2022-05-01 21:30:53 +0600

    Update practice4.py

--- practice4.py
+++ practice4.py
@@ -1,8 +1,18 @@
-#A python program that takes a dictionary from the user and then prints the average of all the values in the dictionary.
-#Sample Input 1: {'Jon': 100, 'Dan':200, 'Rob':300}  Sample Output 1: Average is 200.
-n = 3
-dic = dict(input("Enter: ").split() for i in range(n))
-print(dic)
+#Write Python code of a program that reads an integer, and prints the integer if it is a multiple of either 2 or 5.
+#Input:5 Output: 5.  Input: 3 Output: Not a multiple
+x = 3
+if x% 2 == 0 or x% 5 == 0:
+    print(x)
+else:
+    print("Not a multiple")
+
+#Write Python code of a program that reads an integer, and prints the integer it is a multiple of either 2 or 5 but not both.
+#Input: 10 Output: multiple of 2 and 5 both  Input: 5 Output: 5
+x = 5
+if x% 2 ==0 and x% 5 ==0:
+    print("both")
+else:
+    print(x)
 
 
 
