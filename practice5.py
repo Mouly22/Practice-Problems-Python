3b85c341d72fde3068eb568f1516482a96ee06aa
Mouly <abiraazmary22@gmail.com>
2022-05-14 01:57:40 +0600

    ,

--- practice5.py
+++ practice5.py
@@ -1,14 +1,33 @@
-#Suppose there is a dictionary named exam_marks as given below.
-#exam_marks = {'Cierra Vega': 175, 'Alden Cantrell': 200, 'Kierra Gentry': 165, 'Pierre Cox': 190}
-#Write a Python program that takes an input from the user and creates a new dictionary 
-#with only those elements from 'exam_marks' whose keys have values higher than the user input (inclusive).
-#Sample Input 1:170
-#Sample Output 1: {'Cierra Vega': 175, 'Alden Cantrell': 200, 'Pierre Cox': 190}
-exam_marks = {'Cierra Vega': 175, 'Alden Cantrell': 200, 'Kierra Gentry': 165, 'Pierre Cox': 190}
-p = int(input("enter number: "))
-dict = {}
-for i in exam_marks:
-    if exam_marks[i] >= p:
-        dict[i] = exam_marks[i]
-print(dict)
+#Write the python programs, which prints the following sequences of values in loops:
+#a) 24, 18, 12, 6, 0, -6
+x = 24 
+while x >= -6:
+    if x == -6:
+        print(x, end= " ")
+    else:
+        print(x, end= ", ")
+    x -= 6
+
+    
+#b)-10, -5, 0, 5, 10, 15, 20
+x = -10
+while x <= 20:
+    if x == 20:
+        print(x, end= " ")
+    else:
+        print(x,end = ", ")
+    x += 5
+
+
+#d)18,-27, 36,-45,54,-63
+x = 18
+while x <= 63:
+    if x%2 ==1:
+        if x == 63:
+            print(-x, end =" ")
+        else:
+            print(-x, end= ", ")    
+    else:
+        print(x, end = ", ")
+    x += 9
 
