39930db13d7637ce37eed97f6412e682a8fc73ec
Mouly <abiraazmary22@gmail.com>
2022-06-19 19:29:56 +0600

    🥺

--- *_*78.py
+++ *_*78.py
@@ -1,8 +1,7 @@
 # Suppose you are given a list of contacts as input in single line. Your task is to create a “Phonebook” out of those contacts. The phonebook is nothing but a nested dictionary which is made of 1 rule:
 # 1.You have to save each contact based on the starting letter. If there is no contact starting with a specific letter, that letter should not be shown in the phone book.
 # While printing the phonebook as output the starting letters should be sorted from A-Z. Also the contacts under each letter should be shown in sorted order based on the contact names. See the sample output for clarification.
-# Sample Input:
-# Bob 01932342392 Alice 01546734123 Britney 01303544535 Aeron 01723454642 Smith 01923457890 Tarek 09663922 Carol 01823456785
+# Sample Input: Bob 01932342392 Alice 01546734123 Britney 01303544535 Aeron 01723454642 Smith 01923457890 Tarek 09663922 Carol 01823456785
 # Sample Output: (No need to follow this output format. Just print the resultant dictionary in alphabetical order)
 # {A:{ Aeron:01723454642, Alice:01546734123} B:{Bob:01932342392, Britney:01303544535} C: {Carol:01823456785}S: {Smith:01923457890}T: {Tarek:09663922}}
 def phonebook(*vals):
