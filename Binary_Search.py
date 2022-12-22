3d1704eb2f40d312bc62dc1f9dcea9b3d7320673
Umme Abira Azmary <95493187+Mouly22@users.noreply.github.com>
2022-12-22 23:10:16 +0600

    Update Binary_Search.py

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,18 +1,20 @@
-def binsearch(lst, number):
+def bin_src(lst, val):
     low = 0
-    high = len(lst)-1
-    for i in range(len(lst)):
-        mid = (low + high)//2
+    high = len(lst) -1
+    
+    while True:
+        mid = (low+high)//2
         if low == high:
+            break
             return -1
-        elif lst[mid] == number:
+        elif lst[mid] == val:
             return mid
-        
-        elif lst[mid] > number:
+        elif lst[mid] > val:
             high = mid
-            
         else:
             low = mid + 1
-            
-lst = [0, 2, 5, 7, 10, 35]
-print(binsearch(lst, 1))
+    
+    
+    
+lst = [10, 20, 40, 50, 70, 150, 170, 230]   
+print(bin_src(lst, 70))
