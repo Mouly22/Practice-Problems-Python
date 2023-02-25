f77fbff4d76307d545894d5dd2eb4c23203fb528
Mouly <abiraazmary22@gmail.com>
2023-02-26 03:17:57 +0600

    I/O

--- 11.py
+++ 11.py
@@ -1,5 +1,11 @@
-#Write a python program that prints a square of size N using + where N will be given as input as illustrated in the examples below.
-x = 5
-for i in range(x):
-    y = ("+"* x)
-    print(y)
+inp_file = open('input1a.txt', 'r')
+out_file = open('output1a.txt', 'w')
+val = inp_file.readlines()
+
+for i in range(1, int(val[0])+1):
+    store = val[i].strip()
+    if int(store) % 2 == 0: 
+        out_file.write(f'{store} is an Even number.\n')
+    else:
+        out_file.write(f'{store} is an Odd number.\n')
+inp_file.close()
